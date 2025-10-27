5a29aae9023da8569a190943ba3ebf40e233c035
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-10-27 15:22:28 -0400

    Addded API folder

--- server/fantasyStockLeauge/settings.py
+++ server/fantasyStockLeauge/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig'
+    'catalog.apps.CatalogConfig',
+
+    'api',
 ]
 
 MIDDLEWARE = [
