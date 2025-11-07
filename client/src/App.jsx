63276e5358bf251a2125fbd5a5ba5167156a2a8e
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-11-07 11:53:25 -0500

    Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Signup from "./PublicPages/Signup/signup.jsx"
 import Login from "./PublicPages/Login/login.jsx"
 import Home from "./PrivatePages/Home/home.jsx"
-
+import ProtectedRoute from "./components/protectedRoute.jsx"
 import MatchUp from "./PrivatePages/MatchUp/matchUp.jsx"
 import ExploreStocks from "./PrivatePages/ExploreStocks/exploreStocks.jsx"
 
@@ -14,9 +13,9 @@ function App() {
         <Route path="/Signup" element={<Signup></Signup>}></Route>
         <Route path="/Login" element={<Login></Login>}></Route>
 
-        <Route path="/Private/Home" element={<Home></Home>}></Route>
-        <Route path="/Private/MatchUp" element={<MatchUp></MatchUp>}></Route>
-        <Route path="/Private/ExploreStocks" element={<ExploreStocks></ExploreStocks>}></Route>
+        <Route path="/Private/Home" element={<ProtectedRoute><Home></Home></ProtectedRoute>}></Route>
+        <Route path="/Private/MatchUp" element={<ProtectedRoute><MatchUp></MatchUp></ProtectedRoute>}></Route>
+        <Route path="/Private/ExploreStocks" element={<ProtectedRoute><ExploreStocks></ExploreStocks></ProtectedRoute>}></Route>
       </Routes>
     </BrowserRouter>
   )
